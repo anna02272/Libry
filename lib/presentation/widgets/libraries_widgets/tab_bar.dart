628c674cd8be745cecaf9078e76047b8a36af131
anna02272 <domonjianna@gmail.com>
2024-04-11 14:20:08 +0200

    Design for  tab bar

--- lib/presentation/widgets/libraries_widgets/tab_bar.dart
+++ lib/presentation/widgets/libraries_widgets/tab_bar.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_internship_2024_app/theme.dart';
 
 class TabBarWidget extends StatelessWidget {
   const TabBarWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const TabBar(
-      tabs: <Widget>[
-        Tab(
-          text: 'Most popular packages',
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black.withOpacity(0.1),
+          width: 0.5,
         ),
-        Tab(
-          text: 'Newest packages',
+      ),
+      child: const TabBar(
+        indicator: BoxDecoration(
+          color: Color.fromRGBO(205, 232, 223, 1),
         ),
-      ],
+        indicatorSize: TabBarIndicatorSize.tab,
+        labelColor: textColor,
+        tabs: <Widget>[
+          Tab(
+            text: 'Most popular packages',
+          ),
+          Tab(
+            text: 'Newest packages',
+          ),
+        ],
+      ),
     );
   }
 }
