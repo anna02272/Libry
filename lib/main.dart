7532709dd8a70289cdb12bee89d50b40e8d0aea3
Maja Đukanović <majadjukanovic99@gmail.com>
2024-04-29 09:02:21 +0200

    Develop (#57)
    Fixing onboarding bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
@@ -22,6 +23,7 @@ import 'package:flutter_internship_2024_app/presentation/widgets/bottom_navigati
 import 'package:flutter_internship_2024_app/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 
 
 Future main() async {
@@ -91,6 +93,7 @@ class MyApp extends StatelessWidget {
               LocaleSettings.useDeviceLocale();
             }
             
+            
             return BlocBuilder<ThemeBloc, ThemeState>(
               builder: (context, state) {
                 return TranslationProvider(
@@ -106,7 +109,13 @@ class MyApp extends StatelessWidget {
                         if (snapshot.hasData) {
                           return const BottomNavigation();
                         }
-                        return  const Onbording();
+                        return  Navigator( 
+                        onGenerateRoute: (routeSettings) {
+                          return MaterialPageRoute(
+                            builder: (context) => const Onbording(),
+                          );
+                        },
+                      );
                       },
                     ),
                   ),
